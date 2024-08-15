1b19f13741c4724a7fb4a4d32431d9fdca0d4627
Chandan-MG <chandanmg.98@gmail.com>
2024-08-15 21:46:10 +0530

    new h

--- src/Store/CartProvider.js
+++ src/Store/CartProvider.js
@@ -8,13 +8,11 @@ const defaultCartState= {
 
 const CartReducer = (state, action) => {
     if(action.type === 'ADD'){
-        // const updatedItems = state.items.concat(action.item);
         const updatedTotalAmount = state.totalAmount + action.item.price*action.item.amount;
 
         const existingCartItemIndex = state.items.findIndex(item => item.id === action.item.id);
         const existingCartItem = state.items[existingCartItemIndex];
 
-        // let updatedItem;
         let updatedItems;
 
         if(existingCartItem){
@@ -26,7 +24,6 @@ const CartReducer = (state, action) => {
             updatedItems[existingCartItemIndex] = updatedItem;
         }
         else{
-            // updatedItem = {...action.item};
             updatedItems = state.items.concat(action.item);
         }
         
