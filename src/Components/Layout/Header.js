4fac2acb4b6e1aa2dc1b39e5bc1c8288644f9f0d
Chandan-MG <chandanmg.98@gmail.com>
2024-08-17 22:27:37 +0530

    error

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -6,15 +6,7 @@ import HeaderCartButton from "./HeaderCartButton";
 const Header = (props) => {
 
     return(
-        <Fragment>
-            <header className='header'>
-                <h1>React Meals</h1>
-                <HeaderCartButton onClick= {props.onShowCart} />
-            </header>
-            <div className='main-image'>
-                <img src={TableImage} alt="A table of meals" />
-            </div>
-        </Fragment>
+        
     )
 }
 
